843547de0d1d04430a26f0f23fb64477f06bd63d
Rachid-L <rachid-leboukh_student2021@wilder.school>
2022-04-15 10:57:28 +0200

    userlistPage added

--- client/src/routeur.tsx
+++ client/src/routeur.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./Pages/HomePage";
 import ConnexionPage from "./Pages/ConnexionPage";
+import UserListPage from "./Pages/UserListPage";
 
 function Routeur() {
   return (
@@ -8,6 +9,7 @@ function Routeur() {
       <Routes>
         <Route path="" element={<HomePage />} />
         <Route path="/login" element={<ConnexionPage />} />
+        <Route path="/userlist" element={<UserListPage />} />
       </Routes>
     </BrowserRouter>
   );
