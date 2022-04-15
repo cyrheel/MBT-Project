c61b034a1d3d00de3ddc1d87e044ccc83c38bb12
Cyrheel_ <cyrilb38@hotmail.com>
2022-04-15 11:26:30 +0200

    Ticket creation page added and routeur updated

--- client/src/routeur.tsx
+++ client/src/routeur.tsx
@@ -4,6 +4,7 @@ import ConnexionPage from "./Pages/ConnexionPage";
 import InscriptionPage from "./Pages/InscriptionPage";
 import ProjectCreationPage from "./Pages/ProjetCreationPage";
 import ProjetDetailsPage from "./Pages/ProjetDetailsPage";
+import TicketCreationPage from "./Pages/TicketCreationPage";
 
 function Routeur() {
   return (
@@ -14,6 +15,7 @@ function Routeur() {
         <Route path="/register" element={<InscriptionPage />} />
         <Route path="/projetCreation" element={<ProjectCreationPage />} />
         <Route path="/projetDetails" element={<ProjetDetailsPage />} />
+        <Route path="/ticketCreation" element={<TicketCreationPage />} />
       </Routes>
     </BrowserRouter>
   );
