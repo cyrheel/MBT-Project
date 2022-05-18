14146d7f52260dbf8ef921c0676c66c16182f527
N-akademy <56726632+N-akademy@users.noreply.github.com>
2022-05-18 16:07:40 +0200

    change ticket detail

--- client/src/Pages/TicketDetailPage.tsx
+++ client/src/Pages/TicketDetailPage.tsx
@@ -1,5 +1,20 @@
-function TicketDetailPage(): JSX.Element {
-    return <p>Connexion Page héhé</p>;
-  }
-  
-  export default TicketDetailPage;
+import ReactDOM from 'react-dom';
+import '../Styles/index.css';
+    
+const name = 'Ticket Details';
+const TicketDetailPage = (<>
+  <div>
+    <h2 className="flex items-center justify-center font-medium leading-tight text-4xl mt-0 mb-2 text-blue-600">{name}</h2>
+    <div className="max-w-sm w-full lg:max-w-full lg:flex bg-slate-100">
+      
+    </div>
+      
+  </div>
+</>);
+      
+      
+ReactDOM.render(
+  TicketDetailPage,
+  document.getElementById('root')
+); 
+export default TicketDetailPage;
