35c606645c19a40c0cbdd907c52dce1a775e62f7
Cyrheel_ <cyrilb38@hotmail.com>
2022-05-18 12:00:52 +0200

    add userParamsPage and his route, add Style and Static folder and some basic files inside

---
+++ client/src/Pages/UserParamsPage.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "../Components/Navbar";
+
+function UserParamsPage() {
+  return (
+    <div>
+      <Navbar />
+      <p>Ici la page des paramètre du compte</p>
+    </div>
+  );
+}
+
+export default UserParamsPage;
