4410d961643aa6eb08035e19c337a531c67c9b7e
N-akademy <56726632+N-akademy@users.noreply.github.com>
2022-05-19 12:04:25 +0200

    ticketdetail-firstcommit

--- client/src/Pages/TicketDetailPage.tsx
+++ client/src/Pages/TicketDetailPage.tsx
@@ -1,20 +1,29 @@
-import ReactDOM from 'react-dom';
+import Navbar from '../Components/Navbar';
 import '../Styles/index.css';
     
 const name = 'Ticket Details';
-const TicketDetailPage = (<>
-  <div>
-    <h2 className="flex items-center justify-center font-medium leading-tight text-4xl mt-0 mb-2 text-blue-600">{name}</h2>
-    <div className="max-w-sm w-full lg:max-w-full lg:flex bg-slate-100">
+
+function TicketDetailPage() {
+  
+  return(<>
+      <Navbar/>
+      <div>
+        <h2 className="flex items-center justify-center font-medium leading-tight text-4xl mt-0 mb-2 text-blue-600">
+          {name}nlkmlkm
+        </h2>
+        <div className="max-w-sm w-full lg:max-w-full lg:flex bg-slate-100">
+          <div>
+
+          </div>
+          <div>
+
+          </div>
+
+        </div>
+      </div>
+  </>)
+  
+};
       
-    </div>
-      
-  </div>
-</>);
-      
-      
-ReactDOM.render(
-  TicketDetailPage,
-  document.getElementById('root')
-); 
+
 export default TicketDetailPage;
