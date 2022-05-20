57b36d6812347f486cb50721cafef52241aa01a4
batsh028 <babatsh028@gmail.com>
2022-05-20 10:27:37 +0200

    modif index

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,14 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Routeur from "./routeur";
+import { ApolloClient, InMemoryCache, ApolloProvider, useQuery, gql} from "@apollo/client";
 
 import "./Styles/index.css";
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/',
+  cache: new InMemoryCache()
+});
+
+client
+.query({
+  query: gql`
+    query GetAllProject {
+      project() {
+      }
+    }
+  `
+})
+.then(result => console.log(result));
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <Routeur />
-  </React.StrictMode>
+  <ApolloProvider client={client}>
+    <React.StrictMode>
+      <Routeur />
+    </React.StrictMode>
+  </ApolloProvider>
 );
